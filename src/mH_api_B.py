8a477bb59ae0cb7cbc184bfbbf2f66a741da6349
JSanchez-Posada <julianasanchezposada@gmail.com>
2022-12-09 08:20:42 +0000

    Setting up home wkspc

--- src/mH_api_B.py
+++ src/mH_api_B.py
@@ -25,8 +25,13 @@ stage = '72-74hpf'
 strain = 'myl7:lifeActGFP, fli1a:AcTag-RFP'
 genotype = 'wt'
 no_chs = 2
-dir_channels = Path('D:/Documents JSP/Dropbox/Dropbox_Juliana/PhD_Thesis/Data_ongoing/LS_ongoing/A_LS_Analysis/im_morphoHeart/LS09_F07_V/Im_LS09_F07_V_DS_1100/')
-dir_res = Path('D:/Documents JSP/Dropbox/Dropbox_Juliana/PhD_Thesis/Data_ongoing/LS_ongoing/A_LS_Analysis/im_morphoHeart/LS09_F07_V/')
+
+# function to set up mac or windows
+# dir_channels = Path('D:/Documents JSP/Dropbox/Dropbox_Juliana/PhD_Thesis/Data_ongoing/LS_ongoing/A_LS_Analysis/im_morphoHeart/LS52_F02_V_SR_1029_2A/Im_LS52_F02_V_SR_1029/')
+# dir_res = Path('D:/Documents JSP/Dropbox/Dropbox_Juliana/PhD_Thesis/Data_ongoing/LS_ongoing/A_LS_Analysis/im_morphoHeart/LS52_F02_V_SR_1029_2A/')
+
+dir_channels = Path('/Users/juliana/Dropbox/Dropbox_Juliana/PhD_Thesis/Data_ongoing/LS_ongoing/A_LS_Analysis/im_morphoHeart/LS52_F02_V_SR_1029_2A/Im_LS52_F02_V_SR_1029/')
+dir_res = Path('/Users/juliana/Dropbox/Dropbox_Juliana/PhD_Thesis/Data_ongoing/LS_ongoing/A_LS_Analysis/im_morphoHeart/LS52_F02_V_SR_1029_2A/')
 
 # connection = 24
 fcBasics.alert('woohoo', alert_all)
@@ -46,9 +51,10 @@ print(organ1.__dict__)
 channel_no = 1
 user_chName = 'myocardium'
 im_orientation = 'ventral'
-dir_cho = Path('D:/Documents JSP/Dropbox/Dropbox_Juliana/PhD_Thesis/Data_ongoing/LS_ongoing/A_LS_Analysis/im_morphoHeart/LS09_F07_V/Im_LS09_F07_V_DS_1100/LS09_F07_V_DS_1100_ch0_EDC.tif')
+dir_cho = Path('/Users/juliana/Dropbox/Dropbox_Juliana/PhD_Thesis/Data_ongoing/LS_ongoing/A_LS_Analysis/im_morphoHeart/LS52_F02_V_SR_1029_2A/Im_LS52_F02_V_SR_1029/LS52_F02_V_SR_1029_ch0_EDC.tif')
 resolution = [0.22832596445005054, 0.22832596445005054, 0.652961]
 
+#%%
 
 chMyoc = mCl.ImChannel(channel_no=channel_no, 
                       organ=organ1,
@@ -61,6 +67,7 @@ print(chMyoc.__dict__)
 
 chMyoc.get_images_o()
 
+#%%
 user_meshName = 'myocardium'
 mesh_type = 'tissue'
 mesh_color = 'teal'
@@ -68,7 +75,7 @@ mesh_alpha = 0.1
 contours_source = np.array(range(10))
 
 extractLargest = False
-npy_mesh = np.load(os.path.join(str(organ1.dir_res), 'stacks_npy','LS09_F07_V_DS_1100_s3_ch0_all.npy'))
+npy_mesh = np.load(os.path.join(str(organ1.dir_res), 'stacks_npy','LS52_F02_V_SR_1029_s3_ch0_all.npy'))
 rotateZ_90 = True
 
 m_Myoc = mCl.Mesh_mH(organ=organ1, 
@@ -95,3 +102,5 @@ myoc = m_Myoc.mesh
 m_Myoc.mesh.volume()
 m_Myoc.mesh.area()
 
+
+# %%
