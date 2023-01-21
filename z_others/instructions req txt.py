637e74c49b4fed803dd80c60e7335953a249e9bd
JSanchez-Posada <julianasanchezposada@gmail.com>
2023-01-21 19:31:47 +0000

    requirements.txt

---
+++ z_others/instructions req txt.py
@@ -0,0 +1,4 @@
+#Instructions to get
+# En el terminal moverse a la carpeta de morphoHeart_rev
+# Escribir: (base) JulianasMBP2016:morphoHeart_rev juliana$ source env_mH36/bin/activate
+# (env_mH36) (base) JulianasMBP2016:morphoHeart_rev juliana$ pip freeze > requirements.txt
